957bb58d13d8bce4681c059f28761cb0ebad7a98
Salazar <bethany.miriamx.salazar@intel.com>
2016-08-05 15:52:11 -0500

    TravelRequestApp in progress

---
+++ TravelRequestApp/app/src/main/java/com/example/bsalaz1x/travelrequestapp/TravelRequest.java
@@ -0,0 +1,13 @@
+package com.example.bsalaz1x.travelrequestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TravelRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_request);
+    }
+}
