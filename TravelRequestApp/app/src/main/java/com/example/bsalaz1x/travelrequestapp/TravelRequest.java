a744eac35abe9773e21bb10a01753aadf56b2537
Triampol <alexandra.lesliex.triampol@intel.com>
2016-08-08 15:48:49 -0500

    finished interface

--- TravelRequestApp/app/src/main/java/com/example/bsalaz1x/travelrequestapp/TravelRequest.java
+++ TravelRequestApp/app/src/main/java/com/example/bsalaz1x/travelrequestapp/TravelRequest.java
@@ -1,13 +1,41 @@
 package com.example.bsalaz1x.travelrequestapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class TravelRequest extends AppCompatActivity {
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+public class TravelRequest extends Activity {
+
+    private Button submit;
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_request);
+
+        submit = (Button) findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick (View arg0) {
+
+                Toast.makeText(getApplicationContext(),
+                        "Thank you, your travel request has been submitted.", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
+
+
+
 }
